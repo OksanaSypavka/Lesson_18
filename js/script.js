623c477e9fe8a8014e7167e2d6e563d689c81574
OksanaSypavka <106077915+OksanaSypavka@users.noreply.github.com>
2022-11-11 17:40:26 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*const iconMenu = document.querySelector('.menu_icon');
+if (iconMenu) {
+   const menuBody = document.querySelector('.menu__list');
+   iconMenu.addEventListener("click", function (e) {
+      document.body.classList.toggle('_lock');
+      iconMenu.classList.toggle('_active');
+      menuBody.classList.toggle('_active');
+   })
+
+}*/
